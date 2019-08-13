2a5b75845435f31cd02e3278b51933a523053898
Dumez Theo <dumez.theo.h@gmail.com>
2019-08-13 23:27:31 +0200

    ajout du log-in et sign-up à la home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   root to: 'pages#home'
+
+  devise_scope :user do
+    get "/" => "devise/sessions#new" # custom path to login/sign_in
+    get "/" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+  end
+
+  devise_for :users
+
   resources :missions do
     resources :participations, only: [:new, :create]
   end
+
   get "/dashboard", to:"pages#dashboard"
   #resources :participations, only: [:show, :destroy]
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
