55cc1237a52231f169518fc2667484f6a74ff665
Franck Robin <franck.robin@hotmail.fr>
2019-08-13 17:17:11 +0200

    correction erreur

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
-Mission.destroy_all
 Participation.destroy_all
+Mission.destroy_all
+User.destroy_all
 
 User.create!(
   first_name: 'Satoshi',
