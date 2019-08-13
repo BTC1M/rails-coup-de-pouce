076969a9b5de24a3a63ae7534423a2b38e67d1dd
Joachim Andreault Aiach <joachim.andreault@gmail.com>
2019-08-13 15:56:44 +0200

    mission controller

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,2 +1,47 @@
 class MissionsController < ApplicationController
+  before_action :set_mission, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @missions = Mission.all
+  end
+
+  def show
+  end
+
+  def new
+    @mission = Mission.new
+  end
+
+  def create
+    @mission = Mission.new(mission_params)
+    if @mission.save
+      redirect_to @mission, notice:"mission was successfully created"
+    else
+      render :new
+    end
+  end
+end
+
+#def edit
+ #end
+  #def update
+    #if @mission.update(mission_params)
+      #redirect_to @mission, notice: 'Mission was successfully updated.'
+    #else
+      #render :edit
+    #end
+  #end
+  #def destroy
+    #@mission.destroy
+    #redirect_to missions_url, notice: 'Mission was successfully destroyed.'
+  #end
+
+private
+
+  def mission_params
+    params.require(:mission).permit(:title, :category, :description, :reward, :place, :start_date, :end_date, :number_of_participants)
+  end
+  def set_mission
+    @mission = Mission.find(params[:id])
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :missions
+  has_many :participations
+
+  validates :first_name, uniqueness: { scope: :last_name }
+  validates :birthday, presence: true
 end
