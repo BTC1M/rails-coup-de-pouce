c54007e66bbbf407f109db3713a7d89613c3668b
Dumez Theo <dumez.theo.h@gmail.com>
2019-08-13 18:21:12 +0200

    login_in_root_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
   resources :missions do
     resources :participations, only: [:new, :create]
   end
   get "/dashboard", to:"pages#dashboard"
   #resources :participations, only: [:show, :destroy]
+  devise_for :users, path: 'auth', skip: [:sessions],
+             controllers: {
+                 sessions: 'users/sessions'
+             }
 
+  as :user do
+    get 'auth/sign_in' => 'home#index', as: :new_user_session
+    post 'auth/sign_in' => 'users/sessions#create', as: :user_session
+    delete 'auth/sign_out' => 'users/sessions#destroy', as: :destroy_user_session
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
